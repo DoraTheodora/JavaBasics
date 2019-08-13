093194a2408ad4c405b8a24e1e9f50210e971f53
Theodora <tataru.theodora@yahoo.com>
2019-08-13 16:56:26 +0100

    Update Scanner.java

--- Scanner.java
+++ Scanner.java
@@ -7,7 +7,7 @@ public class MyClass
 		Scanner myInput = new Scanner(System.in); // initiating a new object Scanner
     
 		System.out.println("Please enter your name: ");
-		String myName = myInput.nextLine();
+		String myName = myInput.nextLine(); // reading the input as a String
 		System.out.println("Your name is: " + myName);
 	}
 }
