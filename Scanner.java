216fbdc148f0dc3d39a16e5b1da0b8a74361aa72
Theodora <tataru.theodora@yahoo.com>
2019-08-13 16:55:16 +0100

    Create Scanner.java
    How to get input from a user, using Scanner class from the util* Library

---
+++ Scanner.java
@@ -0,0 +1,13 @@
+import java.util.Scanner; // importing the Scanner - for user input
+
+public class MyClass
+{
+	public static void main(String[] args) 
+	{
+		Scanner myInput = new Scanner(System.in); // initiating a new object Scanner
+    
+		System.out.println("Please enter your name: ");
+		String myName = myInput.nextLine();
+		System.out.println("Your name is: " + myName);
+	}
+}
